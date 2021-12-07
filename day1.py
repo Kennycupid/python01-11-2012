2ce41bf24fee05d748cca2a0459042574f541d47
Kennycupid <95704882+Kennycupid@users.noreply.github.com>
2021-12-07 17:29:24 +0100

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,24 @@
+print('hello world')
+whole_number= 7
+decimal_number= 5
+course= 'python'
+y= 'false'
+print(whole_number)
+print(decimal_number)
+print(course)
+print(y)
+word= 'we\'re brothers from the other side of the town'
+print(word)
+poem='oh beloved nigeria,\nfor long has thou told us we are leaders of tomorrow.\nNow gray hair elude us with your promises yet unfulfilled'
+print(poem)
+print('\n')
+poem2= '''oh beloved nigeria
+for long has thou told us we are leaders of tomorrow
+now gray elude us with your promises yet unfulfilled
+'''
+print(poem2)
+print('Lagos'+ ' '+ 'state') #joining of string and string
+print('hey', course, 'is fun') #joining of variable and string
+Sur_name= 'Agu'
+Last_name= 'kenneth'
+print(Sur_name, Last_name)
